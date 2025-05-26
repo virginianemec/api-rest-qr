269b6a7f96364e23c5b54b4904088bb1f2f6747b
virginianemec <virginia.nemec@gmail.com>
2025-05-26 13:49:27 -0300

    definición de rutas.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-hola
+const express = require('express');
+const multer = require('multer');
+
+const app = express()
+const upload = multer({dest: '/uploads' })
+app.use(express.json);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.post('/imagen', upload.single('imagen'), function(req, res){
+    const body = req.body;
+    const imagen = req.file;
+    console.log(imagen);
+    res.send('esta es el post');
+})
+
+app.listen(3000)
